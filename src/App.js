d9957ba7f9347bcc46c14b092974d2bee7ec0081
Vikas Kumar <89458526+Vika1402@users.noreply.github.com>
2024-05-04 17:13:42 +0530

    workin on newslatter section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Banner from "./components/Banner";
+import Experts from "./components/Experts";
+import Header from "./components/Header";
+import NewsLatter from "./components/NewsLatter";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+<Header/>
+<Banner></Banner>
+<Experts></Experts>
+<NewsLatter/>
+  </>;
 }
 
 export default App;
---
+++ src/components/NewsLatter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NewsLatter = () => {
+  return <div className=" bg-blue-400 p-4">
+    <div className="'max-w-[1240px] flex">
+      <div className="flex1 border h-[300px]">hello</div>
+      <div className="flex2 border h-[300px]">hello</div>
+    </div>
+  </div>;
+};
+
+export default NewsLatter;
